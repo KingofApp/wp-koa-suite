d9f5c7302f0b98e76e2f15a07217f61cbce97d6e
Darryl García <darrylgarcia96@gmail.com>
2022-11-03 18:38:17 +0100

    added events

--- embed/addkoastyle.php
+++ embed/addkoastyle.php
@@ -3,7 +3,6 @@
 add_action( 'after_setup_theme', 'allow_embed', 20);
 
 function allow_embed() {
-    echo $_GET[get_option('koa_embed_allow_embed')];
     /* Adding user styles to head  */
     add_action( 'wp_head', 'add_koa_style' );
      /* Allow woocommerce checkout page to be embeded */
@@ -36,7 +35,27 @@ function add_koa_style() {
           let koa_embed_style = `<?php echo get_option('koa_embed_style'); ?>`;
           var css = koa_embed_style != "" ? koa_embed_style : 'header, footer{ display:none } .elegantshop-products-wrapper{opacity: 1 !important;}';
           addStyle();
+          addEvents();
       }
+	  
+	  function addEvents(){
+		  document.addEventListener("click", function(event){
+			let target = event.path.find((e)=> e.tagName==="A" && e.hasAttribute("download"));
+			if(target){
+				//do stuf here
+				parent.postMessage({toDownload:target.href, preview: true}, "*");
+			}
+		  
+		  	let link = event.path.find((elem)=> elem.tagName==="A" || elem.tagName==="a");
+       
+			if(!link ) return;
+			if(link.getAttribute("target") != "_blank") return;  
+			event.preventDefault();
+		  	parent.postMessage({toOpen: link.href}, "*");
+		  
+		});
+	  }
+      
       function addStyle() {
           var head = document.head || document.getElementsByTagName('head')[0];
           var style = document.createElement('style');
