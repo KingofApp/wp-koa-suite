555a99e50094458104bcb7d452d5765fd3e85977
Darryl García <darrylgarcia96@gmail.com>
2024-08-20 15:46:34 +0200

    added GNU licence now for real

--- wp-koa-suite.php
+++ wp-koa-suite.php
@@ -9,7 +9,24 @@
  * Description: Manage and control all the koa plugins
  * Author: King of app
  * Author URI: https://kingofapp.com/
- */ 
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/**
+ * King of app suite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * King of app suite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with King of app suite. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+ */
 
 /* init */
 if ( is_admin() ) { // admin actions
