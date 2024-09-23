543a1153af7368f45d3e40fbc4dfc09863dba7d1
Darryl García <darrylgarcia96@gmail.com>
2024-09-23 16:10:46 +0200

    removed custom page

--- main/noAdmin.php
+++ main/noAdmin.php
@@ -2,4 +2,3 @@
 include( WP_PLUGIN_DIR.'/koa-suite/embed/addkoastyle.php' );
 include( WP_PLUGIN_DIR.'/koa-suite/qr/qr-endpoint.php' );
 include( WP_PLUGIN_DIR.'/koa-suite/push/apiendpoint/manage_api_endpoint.php' );
-include (WP_PLUGIN_DIR.'/koa-suite/custom_page/custom_page.php');
