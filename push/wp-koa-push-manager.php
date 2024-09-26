100b3aa3d3dc1d03885636940399e54c7873a8e5
Darryl García <darrylgarcia96@gmail.com>
2024-09-26 12:25:01 +0200

    added database creation and settings for the push forms

--- push/wp-koa-push-manager.php
+++ push/wp-koa-push-manager.php
@@ -1,12 +1,48 @@
 <?php
+// Require Composer's autoload file for Google API Client Library
+require_once WP_PLUGIN_DIR.'/koa-suite/push/includes/vendor/autoload.php';
 
-/* init */
-if ( is_admin() ) { // admin actions
+// Define where to store the uploaded JSON credentials
+define('FIREBASE_JSON_CREDENTIALS_PATH', WP_PLUGIN_DIR.'/koa-suite/push/includes/google-credentials.json');
 
-	require_once plugin_dir_path(__FILE__).'configform/form.php';
-	require_once plugin_dir_path(__FILE__).'configform/setings.php';
-	
-} else {
-	require_once plugin_dir_path(__FILE__).'apiendpoint/manage_api_endpoint.php';
-	require_once plugin_dir_path(__FILE__).'automatic_push.php';
+
+add_action( 'admin_init', 'push_setings');
+
+/* Setting the form fields */
+function push_setings() {
+    // Register our setting with a default value
+    register_setting( 'koa-push-settings-group', 'firebase_project_ID', array(
+        'type'              => 'string',
+        'show_in_rest'      => false,
+        'default'           => 'firebase project id',
+    ));
+
+    register_setting( 'koa-push-settings-group', 'koa_push_search', array(
+        'type'              => 'string',
+        'show_in_rest'      => false,
+        'default'           => ""
+    ));
 }
+
+
+//create a table on the database to store all the push 
+function koa_create_push_notifications_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'koa_push_notifications';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id bigint(20) NOT NULL AUTO_INCREMENT,
+        user_id bigint(20) NOT NULL,
+        notification_title varchar(255) NOT NULL,
+        notification_body text NOT NULL,
+        notification_send_date datetime NOT NULL,
+        notification_status varchar(20) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+register_activation_hook( __FILE__, 'koa_create_push_notifications_table' );
