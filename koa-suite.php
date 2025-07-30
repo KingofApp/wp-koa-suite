6609c075c2a7d97f7ee8b0b655849074c0069c5f
Darryl <darrylgarcia96@gmail.com>
2025-07-30 03:29:21 +0200

    set version 1.3.2

--- koa-suite.php
+++ koa-suite.php
@@ -3,7 +3,7 @@
  * Plugin Name:       King of App Suite
  * Plugin URI:        https://kingofapp.com/
  * Description:       Manage and control all the KOA plugins
- * Version:          1.3.1
+ * Version:          1.3.2
  * Requires PHP:      7.2
  * Author:           King of App
  * Author URI:       https://kingofapp.com/
